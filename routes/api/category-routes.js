af50aff576707ee70b0ef68d5be13a52f7047ffc
AntiqueRised <imjoodie@gmail.com>
2023-06-13 14:31:57 -0500

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,16 +34,71 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
-  
+  /* req.body should look like this...
+    {
+      category_name: "Basketball",
+      productIds: [1, 2, 3, 4]
+    }
+  */
+ Category.create(req.body)
+ .then((category) => {
+  if (Array.isArray(req.body.productIds) && req.body.productIds.length) {
+    const categoryTagIdArr = req.body.productIds.map((product_id) => ({
+      category_id: category.id,
+      product_id,
+    }));
+    return Category.bulkCreate(categoryTagIdArr);
+  } else {
+    return null;
+  }
+ })
+ .then(() => {
+  res.status(200).json({ message: 'Category Successfully Created!' });
+ })
+ .catch((err) => {
+  console.log(err);
+  res.status(400).json(err);
+ });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: { 
+      id: req.params.id 
+    },
+  })
+  .then((category) => {
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    return res.status(200).json({ message: 'Category Succesfully Updated!' });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { 
+      id: req.params.id 
+    },
+  })
+  .then((category) => {
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    return res.status(200).json({ message: 'Category Succesfully Deleted!' });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
